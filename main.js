93d34788e35926c121783116aae0fc4e78d00482
Khayreddin-a11y <chaabanikhairedin@gmail.com>
2021-04-15 00:49:02 +0100

    khayri basic architechture for our web game

--- main.js
+++ main.js
@@ -1,1 +1,68 @@
-code
+function imageClass(argument) {
+var imgObj={};
+imgObj.show=false;
+imgObj.mach=false;
+imgObj.click=click;
+}
+var click=function(){
+
+	return this.show=true;
+}
+
+function image(id) {
+    
+	
+   var ab=document.getElementById('ab');
+    if (id==ab)
+	 {
+	 ab.src = "img/1.jpg";}
+
+ var aa=document.getElementById('aa');
+    if (id==aa)
+	 {
+	 aa.src = "img/2.jpg";}
+
+var ac=document.getElementById('ac');
+    if (id==ac)
+	 {
+	 ac.src = "img/3.jpg";}
+
+
+var ad=document.getElementById('ad');
+    if (id==ad)
+	 {
+	 ad.src = "img/4.jpg";}
+
+var ae=document.getElementById('ae');
+    if (id==ae)
+	 {
+	 ae.src = "img/5.jpg";}
+
+
+var abb=document.getElementById('abb');
+    if (id==abb)
+	 {
+	 abb.src = "img/1.jpg";}
+
+ var aaa=document.getElementById('aaa');
+    if (id==aaa)
+	 {
+	 aaa.src = "img/2.jpg";}
+
+var acc=document.getElementById('acc');
+    if (id==acc)
+	 {
+	 acc.src = "img/3.jpg";}
+
+
+var add=document.getElementById('add');
+    if (id==add)
+	 {
+	 add.src = "img/4.jpg";}
+
+var aee=document.getElementById('aee');
+    if (id==aee)
+	 {
+	 aee.src = "img/5.jpg";}
+
+}
